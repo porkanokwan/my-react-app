bdd5b5b86141210a40836732a93a5011c4efdc6e
porkanokwan <kanokwan.duly@gmail.com>
2022-04-29 22:34:30 +0700

    basic-css-with-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// ถ้าเขียน import CSS แบบนี้จะทำให้ไฟล์อื่นหรือ Component อื่น ที่อยู่นอกไฟล์นี้และมี class ชื่่อเดี่ยวกับที่อยู่ในไฟล์ App.css นีจะใช้ CSS จากไฟล์ App.css ได้ด้วย
+// import './App.css';
+import Welcome from './Welcome';
+// ถ้าจะเรียกใช้ CSS จากไฟล์ App.module.css ต้องเรียกใช้ผ่าน styles obj ทำให้การใช้ CSS นี้ใช้ได้เฉพาะในไฟล์นี้เท่านั้น ไฟล์อื่นหรือ Component อื่น Apply เอาไปใช้ไม่ได้
+import styles from './App.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const divStyle = {color: 'red', textAlign: 'center', margin: '20px 10px', padding: 10};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div style = {{color: 'red', textAlign: 'center', margin: '20px 10px', padding: 10}}>Hello React Basic Styling</div>
+    // <div style = {divStyle}>Hello React Basic Styling</div>
+    <div className={styles.container}>
+      <div className = 'main-div'>Hello React Basic Styling</div>
+      {/* <div className = 'text-danger'>Hello React Basic Styling from Bootstrap</div> */}
+      <div className = "alert alert-danger" role="alert">Hello React Basic Styling from Bootstrap</div>
+      {/* เวลาจะเรียกใช้ property ในไฟล์ App.module.css ต้องเรียกใช้ผ่าน styles obj */}
+      <div className = {styles['main-div']}>Hello React Basic Styling</div>
+      <div className = {styles.main}>Hello React Basic Styling</div>
+      <Welcome/>
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,6 @@
+function Welcome() {
+    return <div className='main-div'>This is Welcome Component</div>
+}
+
+// โดยปกติการ export component จะใข้การ export default เพราะ ใน 1 ไฟล์มีได้แค่ 1 default
+export default Welcome;
