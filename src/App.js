bc5881ae3de7605a91006cf4eda4a4fefb8d860d
porkanokwan <kanokwan.duly@gmail.com>
2022-05-01 22:09:17 +0700

    finish lab3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <button className={styles.bu1}>New Game</button>  
+      </div>
+      <div className={styles.container}>
+        <div className={styles['box-1']}>
+          <h1>Player1</h1>
+          <h2>0</h2>
+        </div>
+        <div className={styles['box-2']}>
+          <button className={styles.roll}>Roll</button>
+          <button className={styles.keep}>Keep</button>
+          <div className={styles.score}>1</div>
+          <h3 className={styles.head3}>Round Score: 0</h3>
+        </div>
+        <div className={styles['box-3']}>
+          <h1>Player2</h1>
+          <h2>0</h2>
+        </div>
+      </div>     
     </div>
   );
 }
