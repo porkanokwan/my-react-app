7ca0f8e7f701287ea4542fc291f7e0ff82afa952
porkanokwan <kanokwan.duly@gmail.com>
2022-05-01 20:10:28 +0700

    finish lab 4

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleLink = (event) => {
+    if( window.confirm('Leave for https://www.google.com/ ?') === false ) {
+      event.preventDefault();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href='https://www.google.com/' onClick={handleLink}>Google</a>
     </div>
   );
 }
