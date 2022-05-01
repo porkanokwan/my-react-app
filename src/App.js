f6678a6e38e196c68fbe9fc6a9827a34f88c082b
porkanokwan <kanokwan.duly@gmail.com>
2022-05-01 18:45:34 +0700

    finish lab 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleSelect = (event) => {
+    console.log(event.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onClick={handleSelect}>
+        <optgroup label='country'>
+          <option value='Japan'>Japan</option>
+          <option value='Australia'>Australia</option>
+          <option value='England'>England</option>
+          <option  value='Italy'>Italy</option>
+          <option  value='Taiwan'>Taiwan</option>
+        </optgroup>
+      </select>
     </div>
   );
 }
