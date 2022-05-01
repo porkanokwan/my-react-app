cc64a5818e3afaf7d05b673b37645742c46d2a06
porkanokwan <kanokwan.duly@gmail.com>
2022-05-01 20:35:24 +0700

    finish lab1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div style={{display: 'flex', justifyContent: 'space-evenly', width: '200px', margin:'auto'}}>
+    <div className={styles.main}>
+      <button>+</button>
+      <span>0</span>
+      <button>-</button>
+      <button>Reset</button>
     </div>
   );
 }
