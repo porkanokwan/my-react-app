9cbbed5debc12113bf94d77633b2dffe54c89521
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 15:21:58 +0700

    edit lab2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import {useState} from 'react'
 
 function App() {
-  const [hide, setHide] = useState('');
+  // const [hide, setHide] = useState('');
   
+  // return (
+  //   <div className="App">
+  //     <button onClick={() => setHide('none')}>Click to hide text</button>
+  //     <h1 style={{display: hide}}>Text</h1>
+  //   </div>
+  // );
+  const [show, setShow] = useState(true);
+  
+  const text = show ? <h1>Text</h1> : null;
   return (
     <div className="App">
-      <button onClick={() => setHide('none')}>Click to hide text</button>
-      <h1 style={{display: hide}}>Text</h1>
+      <button onClick={() => setShow(false)}>Click to hide text</button>
+      {text}
     </div>
   );
 }
