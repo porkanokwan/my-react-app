87c034430f879914719d52a6f903d63c75f11a52
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 14:09:51 +0700

    edit lab state1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import './App.css';
 import {useState} from 'react'
 
 function App() {
-  const [show, setShow] = useState(false)
-  const handleButton = () => {
-    setShow(!show)
-  }
-  // const [show, setShow] = useState('')
-  //   const handleButton = () => {
-  //   setShow('hidden')
+  // วิธีนี้จะไม่ได้ลบออกไปจาก DOM แค่ซ่อนไว้
+  // const [hide, setHide] = useState(false)
+  // const handleButton = () => {
+  //   setHide(!hide)
   // }
+  // const [hide, setHide] = useState('')
+  // const handleButton = () => {
+  //   setHide('hidden')
+  // }
+  const [hide, setHide] = useState('inline')
+  const handleButton = () => {
+    setHide('none')
+  }
   return (
     <div className="App">
-      <button onClick={handleButton} hidden={show}>Click to hide me</button>
+      {/* <button onClick={handleButton} hidden={hide}>Click to hide me</button> */}
+      <button onClick={handleButton} style={{display: hide}}>Click to hide me</button>
     </div>
   );
+  
+  
+  // เฉลย
+//   const [show, setShow] = useState(true)
+    // วิธีนี้ remove tag ออกไปจาก DOM เลย
+//   if(show === false) {
+//     return <></>
+//   }
+//   return (
+//     <div className="App">
+//       <button onClick={() => setShow(false)} >Click to hide me</button>
+//     </div>
+//   );
 }
 
 export default App;
