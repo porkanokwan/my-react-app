93297389a04c4aa67948d949e5792337360e70ce
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 15:42:54 +0700

    edit lab4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import {useState} from 'react'
 
 function App() {
   const [cal, setCal] = useState('');
-  const calculate = (dollar) => {
-    return dollar * 30;
-  }
+  // const calculate = (dollar) => {
+  //   return dollar * 30;
+  // }
+  
   return (
     <div className="App">
       <label htmlFor='dollar'>Enter Dollar : </label>
-      <input id='dollar' name='convert-dollar' value={cal} onChange={(e) => setCal(e.target.value)}/>
-      <h2>Convert to Baht: {calculate(cal)} Baht</h2>
+      <input type='text' id='dollar' name='convert-dollar' value={cal} onChange={(e) => setCal(e.target.value)}/>
+      {/* <h2>Convert to Baht: <span style={{color: 'red'}}>{calculate(cal)}</span> Baht</h2> */}
+      <h2>Convert to Baht: <span style={{color: 'red'}}>{cal*30}</span> Baht</h2>
     </div>
   );
 }
