964b29c7e1969fa03a08f6be0f96d30425fa8645
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 15:35:37 +0700

    edit lab3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import {useState} from 'react'
 
 function App() {
   const [change, setChange] = useState('Hide');
-  const [show, setShow] = useState('none')
+  const [show, setShow] = useState('block')
   const handleChange = (e) => {
-    setChange('Show')
-    console.log(change) // เป็น Hide อยู่เพราะเสร็จก่อนจะ Update ค่าเสร็จ
+    // setChange('Show')
+    // console.log(change) // เป็น Hide อยู่เพราะเสร็จก่อนจะ Update ค่าเสร็จ
+    if(change === 'Hide'){
+      setChange('Show');
+      setShow('none');
+    }
     // พอ update ค่าเสร็จจะเข้าเงื่อนไขนี้
     if(change === 'Show'){
       setChange('Hide');
@@ -19,6 +23,18 @@ function App() {
       <h2 style={{display: show}}>Lorem ipsum dolor sit</h2>
     </div>
   );
+  
+  // เฉลย
+  // const [show, setShow] = useState(true);
+  // const btnText = show ? 'Hide' : 'Show';
+  // const text = show ?  <h2>Lorem ipsum dolor sit</h2> : null;
+  // return (
+  //   <div className="App">
+  //     <button onClick={() => setShow(!show)}>{btnText}</button>
+  //     {text}
+  //   </div>
+  // );
+
 }
 
 export default App;
