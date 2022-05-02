fc9fa3f44f684c0b3379de9c6a31b2e9ba609a64
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 08:49:52 +0700

    edit event4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const handleLink = (event) => {
+  //   if( window.confirm('Leave for https://www.google.com/ ?') === false) {
+  //     event.preventDefault();
+  //   }
+  // }
   const handleLink = (event) => {
-    if( window.confirm('Leave for https://www.google.com/ ?') === false ) {
-      event.preventDefault();
+    event.preventDefault();
+    if( window.confirm('Leave for https://www.google.com/ ?')) {
+      // window.location.assign('https://www.google.com/ ')
     }
   }
+  
+  // เฉลย
+  // const handleLink = (event) => {
+  //   event.preventDefault()
+  //   const redirect = window.confirm('Leave for https://www.google.com/ ?');
+  //   if(redirect) {
+  //     window.location.assign('https://www.google.com/ ')
+  //   }
+  // }
   return (
     <div className="App">
       <a href='https://www.google.com/' onClick={handleLink}>Google</a>
