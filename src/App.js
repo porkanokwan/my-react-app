e94a998265192ec7514e3d7c7530c83b06c38631
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 14:02:36 +0700

    finish lab state1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [show, setShow] = useState(false)
+  const handleButton = () => {
+    setShow(!show)
+  }
+  // const [show, setShow] = useState('')
+  //   const handleButton = () => {
+  //   setShow('hidden')
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleButton} hidden={show}>Click to hide me</button>
     </div>
   );
 }
