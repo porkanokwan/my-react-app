118005be0bd118ad0f67d223fcc0a8925f4d2395
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 15:15:35 +0700

    finish lab5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [check, setCheck] = useState('');
+  const [warn, setWarn] = useState('');
+  const [border, setBorder] = useState('');
+  const handleCheck = () => {
+    if(check.length === 0){
+      setWarn('Phone number is required');
+      setBorder('red');
+    } else
+    if(isNaN(check)){
+      setWarn('Phone number is invalid');
+      setBorder('red');
+    } else
+    if(check.length < 10){
+      setWarn('invalid length');
+      setBorder('red');
+    }else {
+      setWarn('');
+      setBorder('');
+      alert('Your number is valid');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type='text' name='phoneNumber' placeholder='Phone Number' style={{borderColor: border}} value={check} onChange={(e) => setCheck(e.target.value)}/>
+        <button onClick={handleCheck}>Check</button>
+      </div>
+      <p style={{color:'red'}}>{warn}</p>
     </div>
   );
 }
