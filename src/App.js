8d045d7cbd65dac0749d83d3b1610eef141ebddf
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 14:48:22 +0700

    finish lab4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [cal, setCal] = useState('');
+  const calculate = (dollar) => {
+    return dollar * 30;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor='dollar'>Enter Dollar : </label>
+      <input id='dollar' name='convert-dollar' value={cal} onChange={(e) => setCal(e.target.value)}/>
+      <h2>Convert to Baht: {calculate(cal)} Baht</h2>
     </div>
   );
 }
