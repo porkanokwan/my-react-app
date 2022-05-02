25f3e07b34dd33414089021cde079e10720d5d06
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 08:38:54 +0700

    edit event3

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ function App() {
   const handleCheckbox = (event) => {
     console.log(event.target.name)
     console.log(event.target.value)
-    console.log('Tick')
+    // checked เป็น attribute ของ input tag ใน html แต่ถ้าใน DOM เป็น property ตัวนึง ใช้ check ว่า checkbox/radio ถูกเลือกมั้ย
+    console.log(event.target.checked ? 'Tick' : 'U ntick')
   }
   return (
     <div className="App">
-      <input type='checkbox' name='phoneBrand' id='phoneBrand' value='iphone' onChange={handleCheckbox} />
-      <label htmlFor='phoneBrand'>Iphone</label>
-      <input type='checkbox' name='phoneBrand' id='phoneBrand' value='samsung' onChange={handleCheckbox}/>
-      <label htmlFor='phoneBrand'>Samsung</label>
-      <input type='checkbox' name='phoneBrand' id='phoneBrand' value='Oppo' onChange={handleCheckbox}/>
-      <label htmlFor='phoneBrand'>Oppo</label>
+      <input type='checkbox' name='phoneBrand' id='iphone' value='iphone' onChange={handleCheckbox} />
+      <label htmlFor='iphone'>Iphone</label>
+      <input type='checkbox' name='phoneBrand' id='samsung' value='samsung' onChange={handleCheckbox}/>
+      <label htmlFor='samsung'>Samsung</label>
+      <input type='checkbox' name='phoneBrand' id='oppo' value='Oppo' onChange={handleCheckbox}/>
+      <label htmlFor='oppo'>Oppo</label>
     </div>
   );
 }
