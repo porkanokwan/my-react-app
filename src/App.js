eeaf2d3fe1c09e1441d5e134d9292402511763d9
porkanokwan <kanokwan.duly@gmail.com>
2022-05-02 22:46:43 +0700

    finish lab1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const arr =['Lin', 'Min', 'Bell'];
+  const li = arr.map((el, id) => <li key={id}>{el}</li>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {li}
+      </ul>
     </div>
   );
 }
