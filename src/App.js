2927bf8b67422f849569faa2d76d280b4d009677
porkanokwan <kanokwan.duly@gmail.com>
2022-05-05 22:21:38 +0700

    intro hook effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState} from 'react'
+
+// Side Effect คือ function ทำสิ่งที่อยู่นอกเหนือ scope ของมัน เช่น
+// let cat = 'Meaow';
+
+// // เปลี่ยนค่าตัวแปรข้างนอก
+// function a() {
+//   cat = 'Mal';
+//   // function ส่ง feature request something to server
+// }
+
+// // Not Side Effect
+// function sum(a, b) {
+//   // ทุกอย่างทำงานใน scope ของ function
+//   return a+b;
+// }
 
 function App() {
+  const [state, setState] = useState({});
+  // Side Effect เพราะ ขอข้อมูลจากภายนอก
+  axios.get('https://jsonplaceholder.typicode.com/todos/1').then(res => {
+    console.log(res.data)
+    // setState(res.data) แบบนี้จะทำให้เกิด Side-effect คือการ re-render แบบ infinite เพราะ เราไป setState ให้มันอัพเดทตลอดเวลา ดังนั้นห้ามใช้ side sffect ใช้ body function component โดยตรง จึงมี Hook useEffect มาจัดการ(handle) side effect ใน body นี้
+  // ดังนั้น ถ้าอยากจะเอาสิ่งที่ res ส่งมา เข้าไปใน state ต้องใช้ useEffect ในการจัดการ เราไม่สามารถยัดเข้า state ตรงๆ แบบนี้ได้
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.title} {/* title โชว์หน้า Web */}
     </div>
   );
 }
