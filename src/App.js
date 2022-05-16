c469f160e26fea78d76cbfa2872da73032967b78
porkanokwan <kanokwan.duly@gmail.com>
2022-05-16 17:21:56 +0700

    finich lab 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import _ from 'lodash';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState({id: _.uniqueId, input: ''});
+  const [isEdit, setEdit] = useState(false);
+
+  const handleAdd = () => {
+    const newList = [...list];
+    newList.push(task);
+    setList(newList);
+    setTask({...task, input: ''});
+  }
   return (
     <div className="App">
-      <input/>
-      <button>Add</button>
+      <input value={task.input} onChange={(e) => setTask(e.target.value)}/>
+      <button onClick={handleAdd}>Add</button>
+
+      <ul>
+        {list.map(item => <li key={item.id} style={{edit}}>{item} <span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </span></li> )}
+      </ul>
     </div>
   );
 }
